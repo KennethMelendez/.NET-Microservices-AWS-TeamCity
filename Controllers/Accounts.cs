1e2a6f6a4cf840e6f1c378ee562177abd2fd1a48
KennethMelendez <kmlndz221@gmail.com.com>
2022-02-07 03:05:39 -0500

    initial commit

---
+++ Controllers/Accounts.cs
@@ -0,0 +1,62 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+
+    /// <summary>
+    /// Account controller for user auth using AWS
+    /// Todo: Put logic in as service class
+    /// </summary>
+    public class Accounts : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public Accounts(SignInManager<CognitoUser> signInManager,
+            UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if(user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists","User with this email already exists");
+                    return View(model);
+
+                }
+
+                user.Attributes.Add(
+                    CognitoAttribute.Name.ToString(),
+                    model.Email);
+
+                var createdUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+                
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
+            return View();
+        }
+    }
+}
